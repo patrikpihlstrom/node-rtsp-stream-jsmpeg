cc72f886ee20026e07214a33cc0e792956ac00fd
patrik <patrik.pihlstrom@northlink.io>
2019-11-28 16:29:23 +0100

    stream to file as well

--- src/configs.js
+++ src/configs.js
@@ -1,6 +1,26 @@
 module.exports = {
 	genFfmpegFormatConfigs: (url, port, options = []) => {
-		options.push(`http://localhost:${port}/s1`);
-		return ["-rtsp_transport", "tcp", "-i", url, '-f', 'mpegts', '-codec:v', 'mpeg1video', '-b:v', '800k', '-r', '30', '-muxdelay', '0.4'].concat(options);
+		let path = null;
+		let pathIndex = options.indexOf('path');
+		if (pathIndex !== -1 && options.length > pathIndex + 1) {
+			path = options[pathIndex + 1];
+			options.splice(pathIndex, 2);
+		}
+
+		if (!path) {
+			path = 'file://tmp/output.mkv';
+		}
+
+		return [
+			'-rtsp_transport', 'tcp',
+			'-i', url,
+			'-codec:v', 'mpeg1video',
+			'-r', '30'
+		].concat(options).concat([
+			'-map', '0:v',
+			'-map', '0:a',
+			'-f', 'mpegts',
+			`tee:${path}|http://localhost:${port}/s1`
+		]);
 	}
 };
